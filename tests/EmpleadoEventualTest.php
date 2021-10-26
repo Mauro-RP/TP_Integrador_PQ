392ef4ea99ffa0558bbb1695a1333f657953af9e
Mauro-RP <mauro.r.prieto@gmail.com>
2021-10-25 21:57:48 -0300

    incorporacion de todos los tests para empleado eventual

--- tests/EmpleadoEventualTest.php
+++ tests/EmpleadoEventualTest.php
@@ -1,19 +1,40 @@
 <?php 
-
 require_once 'EmpleadoTest.php';
 class EmpleadoEventualTest extends EmpleadoTest
 {
-	public function crear($saldo = 5000, $tope=2000,$n=123,$titular="Fulano")
+	private $ventas = array(100,200,300,400,500);
+
+	public function crearEE($nombre, $apellido, $dni, $salario, $montos)
 	{
-		$cc = new \App\EmpleadoEventual($n,$titular,$saldo,$tope);
-		return $cc;
+		$ee = new \App\EmpleadoEventual($nombre,$apellido,$dni,$salario,$montos);
+		return $ee;
 	}
 
-	public function testNoSePuedeExtraerMasDelTopeDescubierto()
+	public function testCalcularComision()
 	{
-		$c = $this->crear(2000,1000);
-		$this->assertEquals("Tope de descubierto excedido",$c -> extraer(4000));
-		$this->assertEquals(2000,$c->getSaldo());
+		$empE = $this->crearEE("Fulano","Cualquiera",36111222,70000,$this->ventas);
+		$com = (1500 / count($this->ventas)) * 0.05;
+		$this->assertEquals($com, $empE -> calcularComision());
+	}
+
+	public function testCalcularIngresoTotal()
+	{
+		$empE = $this->crearEE("Fulano","Cualquiera",36111222,70000,$this->ventas);
+		$this->assertEquals(70000 + $empE -> calcularComision(), $empE -> calcularIngresoTotal());	
+	}
+
+	public function testProbarMontoDeVentaNegativoYCero()
+	{
+		$this->expectException(\Exception::class);
+		$vent = array(100,200,300,-400,500);
+		$empE = $this->crearEE("Fulano","Cualquiera",36111222,70000,$vent);
+	}
+
+	public function testProbarMontoDeVentaCero()
+	{
+		$this->expectException(\Exception::class);
+		$vents = array(100,200,300,0,500);
+		$empEv = $this->crearEE("Fulano","Cualquiera",36111222,70000,$vents);
 	}
 
 }
